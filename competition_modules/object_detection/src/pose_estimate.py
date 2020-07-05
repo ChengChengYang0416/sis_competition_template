f63fcd1427daf3858bd153f0b0f920d0f2119d3b
ChengChengYang1997 <kb240608@gmail.com>
2020-07-05 12:41:34 +0800

    modified pose_estimate.py

--- competition_modules/object_detection/src/pose_estimate.py
+++ competition_modules/object_detection/src/pose_estimate.py
@@ -236,9 +236,9 @@ def rosRGBDCallBack(rgb_data, depth_data):
 		    xyz = np.array([xyz_blue[2], -xyz_blue[0], -xyz_blue[1], 1])
                     final_xyz = matrix.dot(xyz)
 
-		    matrix_rot_x = np.array([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-		    final_xyz_rot_x = matrix_rot_x.dot(final_xyz)
-            	    print(final_xyz_rot_x)
+		    #matrix_rot_x = np.array([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+		    #final_xyz_rot_x = matrix_rot_x.dot(final_xyz)
+            	    print(final_xyz)
 
             	    command = Pose()
             	    command.position.x = xyz_blue[0]
