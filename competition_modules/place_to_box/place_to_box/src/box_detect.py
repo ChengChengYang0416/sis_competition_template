fc694de649fc3802dd98b2b3ba97918fa5e58505
ChengChengYang1997 <kb240608@gmail.com>
2020-07-06 01:38:32 +0800

    modified place_client

--- competition_modules/place_to_box/place_to_box/src/box_detect.py
+++ competition_modules/place_to_box/place_to_box/src/box_detect.py
@@ -9,7 +9,7 @@ import cv2
 
 def detect():
     originalImage = cv2.imread('test.png')
-    grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
+    grayImage = cv2.cvtColor(originalImage, cv2.CV_BGR2GRAY)
     (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
     cv2.imshow('Black white image', blackAndWhiteImage)
 
