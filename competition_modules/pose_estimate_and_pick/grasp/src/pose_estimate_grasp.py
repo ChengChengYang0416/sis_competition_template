617ef59189548aec933a8c7e73e2db176130c4e3
ChengChengYang1997 <kb240608@gmail.com>
2020-07-05 17:54:33 +0800

    modified pose_estimate_grasp.py

--- competition_modules/pose_estimate_and_pick/grasp/src/pose_estimate_grasp.py
+++ competition_modules/pose_estimate_and_pick/grasp/src/pose_estimate_grasp.py
@@ -267,10 +267,10 @@ def rosRGBDCallBack(rgb_data, depth_data):
 		    global counter
 		    if (final_xyz[0] <= 0.45):
 			final_xyz_re = np.array([final_xyz[0]+0.1, -final_xyz[1]+0.02, 0.05])
-			if (angle_pre >= 90):
-			    orien = np.array([ -0.743098, -0.2559235, -0.4145385, -0.4587661 ])
+			if (angle_pre <= 90):
+			    orien = np.array([0.2705981, 0.6532815, -0.2705981, 0.6532815])
 			else:
-			    orien = np.array([ -0.743098, 0.2559235, 0.4145385, -0.4587661 ])
+			    orien = np.array([-0.2705981, 0.6532815, 0.2705981, 0.6532815])
                         #orien = np.array([0.0, 0.0, 0.0, 0.0])
             	    	print(final_xyz_re)
 			counter = counter + 1
